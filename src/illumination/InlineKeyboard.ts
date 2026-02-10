777bc339270fc64ff55a67d59f438b7f1d867fb8
2byte <oopsfix21@gmail.com>
2026-02-10 12:40:36 +0300

    bump version

--- src/illumination/InlineKeyboard.ts
+++ src/illumination/InlineKeyboard.ts
@@ -4,6 +4,8 @@ import { Section } from "./Section";
 export class InlineKeyboard {
   private keyboard: any[][] = [];
   private footFixedButtons: any[][] = [];
+  private appendIndexes: number[] = [];
+  private prependIndexes: number[] = [];
 
   static init(ctx: Telegraf2byteContext, section: Section): InlineKeyboard {
     return new InlineKeyboard(ctx, section);
@@ -22,24 +24,30 @@ export class InlineKeyboard {
     return this;
   }
 
-  append(row: any[] | any[][] | any): InlineKeyboard {
+  append(...row: any[] | any[][] | any): InlineKeyboard {
     if (!Array.isArray(row)) {
       this.keyboard.push([row]);
+      this.appendIndexes.push(this.keyboard.length - 1);
     } else if (Array.isArray(row[0])) {
       this.keyboard.push(...row);
+      this.appendIndexes.push(...Array.from({ length: row.length }, (_, i) => this.keyboard.length - row.length + i));
     } else {
       this.keyboard.push(row);
+      this.appendIndexes.push(this.keyboard.length - 1);
     }
     return this;
   }
 
-  prepend(row: any[]): InlineKeyboard {
+  prepend(...row: any[]): InlineKeyboard {
     if (!Array.isArray(row)) {
       this.keyboard.unshift([row]);
+      this.prependIndexes.push(0);
     } else if (Array.isArray(row[0])) {
       this.keyboard.unshift(...row);
+      this.prependIndexes.push(...Array.from({ length: row.length }, (_, i) => i));
     } else {
       this.keyboard.unshift(row);
+      this.prependIndexes.push(0);
     }
     return this;
   }
@@ -57,5 +65,15 @@ export class InlineKeyboard {
 
   [Symbol.toPrimitive]() {
     return this.valueOf();
+  }
+
+  reset(): InlineKeyboard {
+    const appendsPrepends = [...this.appendIndexes, ...this.prependIndexes].sort((a, b) => a - b);
+    for (let i = appendsPrepends.length - 1; i >= 0; i--) {
+      this.keyboard.splice(appendsPrepends[i], 1);
+    }
+    this.appendIndexes = [];
+    this.prependIndexes = [];
+    return this;
   }
 }
